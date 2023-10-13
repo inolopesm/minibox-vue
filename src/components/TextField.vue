0fc9ce8b954834f614c600a21c3f8109d7ed7bcd
Matheus Lopes <inolopesm@gmail.com>
2023-10-13 08:16:43 -0300

    disabled text field

--- src/components/TextField.vue
+++ src/components/TextField.vue
@@ -1,16 +1,23 @@
 <script setup lang="ts">
-defineProps<{ label: string; value: string; type: "text" | "password" }>();
+defineProps<{
+  label: string;
+  value: string;
+  type: "text" | "password";
+  disabled?: boolean;
+}>();
+
 defineEmits<{ (event: "update:value", value: string): void }>();
 </script>
 
 <template>
   <div>
-    <div class="mb-2">{{ label }}</div>
+    <div :class="['mb-2', disabled && 'text-gray-300']">{{ label }}</div>
     <input
       :type="type"
-      class="block w-full rounded border-2 border-gray-300 bg-white px-3 py-4 transition focus:border-blue-700 focus:outline-none"
+      class="block w-full rounded border-2 border-gray-300 bg-white px-3 py-4 transition focus:border-blue-700 focus:outline-none disabled:text-gray-300"
       :value="value"
       @input="$emit('update:value', ($event.target as HTMLInputElement).value)"
+      :disabled="disabled"
     />
   </div>
 </template>
