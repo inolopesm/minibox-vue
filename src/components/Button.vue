ee14569dd8a8f091c1bb0393ce63b4b7b1482a03
Matheus Lopes <inolopesm@gmail.com>
2023-10-13 08:16:37 -0300

    disabled button

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 withDefaults(
-  defineProps<{ variant?: "primary" | "secondary" | "tertiary" }>(),
+  defineProps<{
+    variant?: "primary" | "secondary" | "tertiary";
+    disabled?: boolean;
+  }>(),
   { variant: "primary" }
 );
 
@@ -13,13 +16,14 @@ defineEmits<{ (event: "click"): void }>();
     :class="[
       'inline-flex min-w-[4rem] cursor-pointer items-center justify-center rounded border px-2 py-1.5 text-sm font-medium uppercase leading-7 transition',
       variant === 'primary' &&
-        'border-blue-700 bg-blue-700 text-gray-50 shadow hover:border-blue-800 hover:bg-blue-800 hover:shadow-lg',
+        'border-blue-700 bg-blue-700 text-gray-50 shadow hover:border-blue-800 hover:bg-blue-800 hover:shadow-lg disabled:border-gray-300 disabled:bg-gray-300 disabled:text-gray-400 disabled:shadow-none',
       variant === 'secondary' &&
-        'border-blue-700/50 bg-transparent text-blue-700 hover:border-blue-700 hover:bg-blue-50',
+        'border-blue-700/50 bg-transparent text-blue-700 hover:border-blue-700 hover:bg-blue-50 disabled:border-gray-300 disabled:text-gray-400',
       variant === 'tertiary' &&
-        'border-transparent bg-transparent text-blue-700 hover:border-blue-50 hover:bg-blue-50',
+        'border-transparent bg-transparent text-blue-700 hover:border-blue-50 hover:bg-blue-50 disabled:border-gray-300 disabled:text-gray-400',
     ]"
     @click="$emit('click')"
+    :disabled="disabled"
   >
     <slot></slot>
   </button>
