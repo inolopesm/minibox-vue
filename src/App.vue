076f142ca9b05f0acc644df068591a5766374df2
Matheus Lopes <inolopesm@gmail.com>
2023-10-13 09:07:53 -0300

    store globalState in cookies

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts">
-import { provide, reactive } from "vue";
+import { provide, reactive, watch } from "vue";
+import Cookies from "js-cookie";
 
 export interface GlobalState {
   accessToken: string;
 }
 
-const globalState = reactive<GlobalState>({ accessToken: "" });
+const globalState = reactive<GlobalState>(
+  JSON.parse(Cookies.get("minibox") ?? "null") ?? { accessToken: "" }
+);
+
+watch(globalState, (state) =>
+  Cookies.set("minibox", JSON.stringify(state), {
+    path: "/",
+    secure: window.location.protocol === "https:",
+    sameSite: "strict",
+  })
+);
 
 provide("globalState", globalState);
 </script>
